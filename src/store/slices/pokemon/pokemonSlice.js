adb2f5bee647381ee7d3405beff3b4aa70d3c202
d3us <dnarino@gmail.com>
2023-09-07 15:25:50 -0500

    create pokemonSlice

---
+++ src/store/slices/pokemon/pokemonSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const pokemonSlice = createSlice({
+    name: 'pokemon',
+    initialState: {
+        page: 0,
+        pokemons: [],
+        isLoading: false
+    },
+    reducers: {
+        increment: (state, /* action */) => {
+            state.counter += 1;
+        },
+    }
+});
+
+
+export const { increment } = pokemonSlice.actions;
